eed633245992b2c3db2f822e6902ebbfbbeb7c43
Matthew Kennedy <kennedymj97@gmail.com>
2020-11-29 21:27:24 +0000

    attempt 2 at getting google analytics working

---
+++ src/components/GoogleAnalytics.svelte
@@ -0,0 +1,27 @@
+<svelte:head>
+  <script async src="https://www.googletagmanager.com/gtag/js?id={id}"></script>
+</svelte:head>
+
+<script>
+  export let stores;
+  export let id;
+
+  if (typeof window !== "undefined") {
+    window.dataLayer = window.dataLayer || []
+	window.gtag = function gtag() {
+	  window.dataLayer.push(arguments)
+	}
+	window.gtag("js", new Date())
+	window.gtag("config", id, { 'send_page_view': false })
+  }
+
+  const { page } = stores();
+
+  $: {
+    if (typeof gtag !== "undefined"){
+	  window.gtag("config", id, {
+	    page_path: $page.path,
+	  });
+    }
+  }
+</script>
--- src/routes/_layout.svelte
+++ src/routes/_layout.svelte
@@ -1,9 +1,12 @@
 <script>
 	import Nav from '../components/Nav.svelte';
+	import GoogleAnalytics from '../components/GoogleAnalytics.svelte';
+	import { stores } from '@sapper/app';
 
 	export let segment;
 </script>
 
+<GoogleAnalytics {stores} id={"G-JCJ44Y6QZY"} />
 <div class="relative flex flex-col p-6 mx-auto max-w-screen-lg md:flex-row">
   <Nav {segment}/>
   <main class="flex-grow md:ml-40 lg:ml-56">
