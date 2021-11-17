33fcf9adbdfbd13ce2b441a2407008761738053f
Matthew Kennedy <kennedymj97@gmail.com>
2021-11-17 18:29:55 +0000

    remove contact page, some links no longer open new tab by default

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -16,10 +16,9 @@
 		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
 		<li class="my-2"><a class=nav-item rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">Blog</a></li>
-		<li class="my-2"><a class=nav-item aria-current="{segment === 'contact' ? 'page' : undefined}" href="contact">Contact</a></li>
         <div class="flex items-center mt-4">
-            <a target="_blank" href="https://twitter.com/kennedymj97" class="cursor-pointer"><img alt="Twitter logo" src="./Twitter_Logo_WhiteOnBlue.svg" class="h-6 mr-2" /></a> 
-            <a target="_blank" href="https://github.com/kennedymj97" class="cursor-pointer"><img alt="GitHub logo" src="./GitHub-Mark-64px.png" class="h-6" /></a> 
+            <a href="https://twitter.com/kennedymj97" class="cursor-pointer"><img alt="Twitter logo" src="./Twitter_Logo_WhiteOnBlue.svg" class="h-6 mr-2" /></a> 
+            <a href="https://github.com/kennedymj97" class="cursor-pointer"><img alt="GitHub logo" src="./GitHub-Mark-64px.png" class="h-6" /></a> 
         </div>
 	</ul>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,3 @@
-<script>
-  import successkid from "images/successkid.jpg";
-</script>
-
 <svelte:head>
   <title>Matthew Kennedy | Home</title>
 </svelte:head>
@@ -9,5 +5,5 @@
 <div class="flex flex-col max-w-lg mx-auto">
   <img src="./portrait.jpg" alt="portrait" class="self-center w-32 h-32 mb-4 rounded-full" />
   <p class="mb-4">Hi, my name is Matt, I'm a software developer from the UK. I'm interested in all things software. This site includes the <a class="link" href="/projects">projects</a> and <a class="link" href="/blog">blog posts</a> I have decided to share publicly.</p>
-  <p>I'm currently looking for a job. If you have an interesting opportunity <a class="link" href="/contact">get in touch</a>.
+  <p>Currently working at Wise</p>
 </div>
