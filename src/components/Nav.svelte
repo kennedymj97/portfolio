da51106b7a4fc5143d4d434230f340d95533d188
Matt Kennedy <mattkennedy@192.168.1.118>
2026-02-13 19:21:33 +0000

    Add About heading and remove social links from navigation

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,24 +1,36 @@
 <script>
-	export let segment;
+  export let segment;
 </script>
 
 <style lang="postcss">
-    .nav-item {
-        @apply font-semibold text-lg text-gray-700 hover-hover:hover:text-indigo-700 hover-hover:hover:underline;
-    }
+  .nav-item {
+    @apply font-semibold text-lg text-gray-700 hover-hover:hover:text-indigo-700 hover-hover:hover:underline;
+  }
 </style>
 
 <nav class="w-40 mb-10 md:fixed md:mb-0">
-    <span class="text-3xl font-bold">Matthew Kennedy</span>
-	<ul class="mt-6">
-		<li class="my-2"><a class=nav-item aria-current="{segment === undefined ? 'page' : undefined}" href=".">Home</a></li>
-		<li class="my-2"><a class=nav-item aria-current="{segment === 'projects' ? 'page' : undefined}" href="projects">Projects</a></li>
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
+  <span class="text-3xl font-bold">Matthew Kennedy</span>
+  <ul class="mt-6">
+    <li class="my-2">
+      <a
+        class="nav-item"
+        aria-current={segment === undefined ? 'page' : undefined}
+        href=".">Home</a>
+    </li>
+    <li class="my-2">
+      <a
+        class="nav-item"
+        aria-current={segment === 'projects' ? 'page' : undefined}
+        href="projects">Projects</a>
+    </li>
+    <!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
 		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li class="my-2"><a class=nav-item rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">Blog</a></li>
-        <div class="flex items-center mt-4">
-            <a href="https://twitter.com/kennedymj97" class="cursor-pointer"><img alt="Twitter logo" src="./Twitter_Logo_WhiteOnBlue.svg" class="h-6 mr-2" /></a> 
-            <a href="https://github.com/kennedymj97" class="cursor-pointer"><img alt="GitHub logo" src="./GitHub-Mark-64px.png" class="h-6" /></a> 
-        </div>
-	</ul>
+    <li class="my-2">
+      <a
+        class="nav-item"
+        rel="prefetch"
+        aria-current={segment === 'blog' ? 'page' : undefined}
+        href="blog">Blog</a>
+    </li>
+  </ul>
 </nav>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,13 +2,12 @@
   <title>Matthew Kennedy | Home</title>
 </svelte:head>
 
-<div class="flex flex-col max-w-lg mx-auto">
-  <p class="mb-4">
-    Hi, my name is Matt, I'm a software developer from the UK. I'm interested in
-    all things software. This site includes the
-    <a class="link" href="/projects">projects</a>
-    and
-    <a class="link" href="/blog">blog posts</a>
-    I have decided to share publicly.
-  </p>
-</div>
+<h1>About</h1>
+<p class="mb-4">
+  Hi, my name is Matt, I'm a software developer from the UK. I'm interested in
+  all things software. This site includes the
+  <a class="link" href="/projects">projects</a>
+  and
+  <a class="link" href="/blog">blog posts</a>
+  I have decided to share publicly.
+</p>
