a42c3b4856f41dc6036934081343583808ff643d
Matt Kennedy <mattkennedy@192.168.1.118>
2026-02-13 19:06:02 +0000

    Update PostCSS to fix build error and remove Wise/portrait from home page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,7 +3,12 @@
 </svelte:head>
 
 <div class="flex flex-col max-w-lg mx-auto">
-  <img src="./portrait.jpg" alt="portrait" class="self-center w-32 h-32 mb-4 rounded-full" />
-  <p class="mb-4">Hi, my name is Matt, I'm a software developer from the UK. I'm interested in all things software. This site includes the <a class="link" href="/projects">projects</a> and <a class="link" href="/blog">blog posts</a> I have decided to share publicly.</p>
-  <p>Currently working at Wise</p>
+  <p class="mb-4">
+    Hi, my name is Matt, I'm a software developer from the UK. I'm interested in
+    all things software. This site includes the
+    <a class="link" href="/projects">projects</a>
+    and
+    <a class="link" href="/blog">blog posts</a>
+    I have decided to share publicly.
+  </p>
 </div>
